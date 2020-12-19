31656d2830a8aaef6c84050d8e4338b96c036ad2
vikalpgaur <vikalpgaur@gmail.com>
2020-12-19 11:25:26 +0530

    First commit
    Added project to github

---
+++ Game Prototype/Prototype/Assets/Scripts/Jump.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private void Update()
+    {
+        getJump();
+    }
+    void getJump()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Game Prototype/Prototype/Assets/Scripts/LaunchManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class LaunchManager : MonoBehaviour
+{
+    public GameObject mainMenupanel;
+    public GameObject levelSelectPanel;
+
+    void Start()
+    {
+        mainMenupanel.SetActive(true);
+        levelSelectPanel.SetActive(false);
+    }
+
+    public void playButton()
+    {
+        mainMenupanel.SetActive(false);
+        levelSelectPanel.SetActive(true);
+    }
+
+    public void quitButton()
+    {
+        Application.Quit();
+        Debug.Log("It works");
+    }
+
+    public void backButton()
+    {
+        levelSelectPanel.SetActive(false);
+        mainMenupanel.SetActive(true);
+    }
+
+    public void level1()
+    {
+        levelSelectPanel.SetActive(false);
+
+    }
+}
